5b3ab823df3d93f35a6e8c0da368a759b2adddaa
Práger Gergő <prager.gergo@gmail.com>
2024-05-19 17:43:04 +0200

    ui module

--- src/app/components/ui/ui.module.ts
+++ src/app/components/ui/ui.module.ts
@@ -5,24 +5,39 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import {MatInputModule} from '@angular/material/input';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    PrimaryButtonComponent
+    PrimaryButtonComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    RouterModule,
   ],
   exports: [
     PrimaryButtonComponent,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    NavbarComponent,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    RouterModule
   ]
 })
 export class UiModule { }
