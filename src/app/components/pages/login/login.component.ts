277c75497c7ee5a943f0933934e4ff5e3f0580f4
Práger Gergő <prager.gergo@gmail.com>
2024-05-19 17:42:42 +0200

    login page

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'login-page',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginPageComponent {
+  loginForm: FormGroup;
+  constructor(private authService: AuthService) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
 
+  login() {
+    const { email, password } = this.loginForm.value;
+    this.authService.login(email, password);
+  }
 }
