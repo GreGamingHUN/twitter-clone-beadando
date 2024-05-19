f8265b57422920ed58b14d4d0f3d6bde13b77b4b
Práger Gergő <prager.gergo@gmail.com>
2024-05-19 18:59:30 +0200

    new post page

---
+++ src/app/components/pages/new-post/new-post-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-post-page',
+  templateUrl: './new-post-page.component.html',
+  styleUrl: './new-post-page.component.scss'
+})
+export class NewPostPageComponent {
+
+}
